e82f2695f0fd0a244061c51c86c116d9fa1413ea
Bob Summerwill <bob@summerwill.net>
2015-12-04 04:45:42 -0800

    Leave sudo-ing to the outside.

--- build-armel.sh
+++ build-armel.sh
@@ -1,1 +1,1 @@
-sudo TARGET_ARCHITECTURE="arm-unknown-linux-gnueabi" docker build -f Dockerfile-eth -t cross-eth .
+TARGET_ARCHITECTURE="arm-unknown-linux-gnueabi" docker build -f Dockerfile-eth -t cross-eth .
