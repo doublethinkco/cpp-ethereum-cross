f6e8e26fc2d0ed046cf9250a35d1b1ec4622c172
Bob Summerwill <bob@summerwill.net>
2015-12-16 20:32:52 -0800

    Added shell scripts for "apt" built-in-compiler variants.

--- build-armhf-apt.sh
+++ build-armhf-apt.sh
@@ -3,5 +3,5 @@ DEST_DIR=$1; DEST_DIR=${DEST_DIR:="/tmp"}
 ./build-generic.sh \
   "crosseth" \
   "${DEST_DIR?}" \
-  "./cross-build/ethereum/main.sh" "armel" "manual"
+  "./cross-build/ethereum/main.sh" "armhf" "apt"
 
