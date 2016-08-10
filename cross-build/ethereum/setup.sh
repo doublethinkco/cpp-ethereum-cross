11c1ae9cd4a45808d867a5a7607abdcfd87e14ab
yep <yep171@yahoo.com>
2016-08-10 13:00:22 +0200

    Set AUTOCONF_ARCHITECTURE, GCC_CROSS_COMPILER/GXX_CROSS_COMPILER for iOS

--- cross-build/ethereum/setup.sh
+++ cross-build/ethereum/setup.sh
@@ -57,12 +57,21 @@ export readonly  LIBJSON_RPC_CPP_DOWNLOAD_URL="https://github.com/doublethinkco/
 export readonly              MHD_DOWNLOAD_URL="http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-${MHD_VERSION?}.tar.gz"
 
 # ===========================================================================
-export readonly AUTOCONF_BUILD_ARCHITECTURE="${ORIGIN_ARCHITECTURE?}-linux-gnu"
+if [ "${CROSS_COMPILER_PROVENANCE?}" == "xcode" ]; then
+  export readonly AUTOCONF_BUILD_ARCHITECTURE="${ORIGIN_ARCHITECTURE?}-apple-darwin"
+else
+  export readonly AUTOCONF_BUILD_ARCHITECTURE="${ORIGIN_ARCHITECTURE?}-linux-gnu"
+fi
 
 if [ "${TARGET_SUBTYPE?}" == "armel" ]; then
   export readonly AUTOCONF_HOST_ARCHITECTURE="${TARGET_ARCHITECTURE?}-unknown-linux-gnueabi"
-else
+elif [ "${TARGET_SUBTYPE?}" == "armhf" ]; then
   export readonly AUTOCONF_HOST_ARCHITECTURE="${TARGET_ARCHITECTURE?}-unknown-linux-gnueabihf"
+elif [ "${TARGET_SUBTYPE?}" == "armv7" ]; then
+  export readonly AUTOCONF_HOST_ARCHITECTURE="${TARGET_SUBTYPE?}-apple-darwin"
+else
+  echo "ERROR: invalid TARGET_SUBTYPE '${TARGET_SUBTYPE}'"
+  exit 1
 fi
 
 # ===========================================================================
@@ -70,14 +79,14 @@ if [ "${CROSS_COMPILER_PROVENANCE?}" == "apt" ]; then
   export readonly CROSS_COMPILER_ROOT_DIR="/usr"
 
   if [ "${TARGET_SUBTYPE?}" == "armel" ]; then
-    export readonly  GCC_CROSS_COMPILER="/usr/bin/arm-linux-gnueabi-gcc"
-    export readonly  GXX_CROSS_COMPILER="/usr/bin/arm-linux-gnueabi-g++"
+    export readonly GCC_CROSS_COMPILER="/usr/bin/arm-linux-gnueabi-gcc"
+    export readonly GXX_CROSS_COMPILER="/usr/bin/arm-linux-gnueabi-g++"
   else
-    export readonly  GCC_CROSS_COMPILER="/usr/bin/arm-linux-gnueabihf-gcc"
-    export readonly  GXX_CROSS_COMPILER="/usr/bin/arm-linux-gnueabihf-g++"
+    export readonly GCC_CROSS_COMPILER="/usr/bin/arm-linux-gnueabihf-gcc"
+    export readonly GXX_CROSS_COMPILER="/usr/bin/arm-linux-gnueabihf-g++"
   fi
 
-else
+elif [ "${CROSS_COMPILER_PROVENANCE?}" == "manual" ]; then
   export readonly XCOMPILER_VERSION="15-12-04"
   export readonly XCOMPILER_DOWNLOAD_URL="https://github.com/doublethinkco/webthree-umbrella-cross/releases/download"
   export readonly XCOMPILER_DESTINATION_DIR="$HOME/x-tools"
@@ -92,9 +101,24 @@ else
 
   export readonly CROSS_COMPILER_TARGET=$(echo "${CROSS_COMPILER_ROOT_DIR?}" | awk -F$'/' '{print $NF}')
 
-  export readonly  GCC_CROSS_COMPILER="${CROSS_COMPILER_ROOT_DIR?}/bin/${CROSS_COMPILER_TARGET?}-gcc"
-  export readonly  GXX_CROSS_COMPILER="${CROSS_COMPILER_ROOT_DIR?}/bin/${CROSS_COMPILER_TARGET?}-g++"
+  export readonly GCC_CROSS_COMPILER="${CROSS_COMPILER_ROOT_DIR?}/bin/${CROSS_COMPILER_TARGET?}-gcc"
+  export readonly GXX_CROSS_COMPILER="${CROSS_COMPILER_ROOT_DIR?}/bin/${CROSS_COMPILER_TARGET?}-g++"
+
+elif [ "${CROSS_COMPILER_PROVENANCE?}" == "xcode" ]; then
+  export readonly PLATFORM="iPhoneSimulator"  # supported values are: AppleTVOS AppleTVSimulator MacOSX WatchOS WatchSimulator iPhoneOS iPhoneSimulator
+  export readonly XCODE_ROOT=$(xcode-select -print-path)
+  export readonly PLATFORM_ROOT="${XCODE_ROOT}/Platforms/${PLATFORM}.platform/Developer"
+  export readonly SDK_ROOT="${PLATFORM_ROOT}/SDKs/${PLATFORM}.sdk"
+  export readonly TOOLCHAIN_ROOT="${XCODE_ROOT}/Toolchains/XcodeDefault.xctoolchain/usr/bin/"
+
+  export readonly GCC_CROSS_COMPILER="${TOOLCHAIN_ROOT}/clang"
+  export readonly GXX_CROSS_COMPILER="${TOOLCHAIN_ROOT}/clang++}"
+
+else
+  echo "ERROR: invalid CROSS_COMPILER_PROVENANCE '${CROSS_COMPILER_PROVENANCE}'"
+  exit 1
 fi
+
 export readonly GCC_CROSS_COMPILER_PATTERN=$(perl -e "print quotemeta('${GCC_CROSS_COMPILER}')")
 export readonly GXX_CROSS_COMPILER_PATTERN=$(perl -e "print quotemeta('${GXX_CROSS_COMPILER}')")
 export readonly CMAKE_TOOLCHAIN_FILE="${INSTALLS_DIR?}/cmake/toolchain"
